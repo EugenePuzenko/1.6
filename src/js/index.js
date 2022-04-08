33e1acde526f5a0360885480b0e2b96866596da9
Eugene Puzenko <puzzenko89@gmail.com>
2022-04-08 19:33:27 +0300

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,267 @@
+import 'swiper/css';
+import 'swiper/css/pagination';
+import Swiper, { Pagination } from 'swiper';
+
+Swiper.use({ Pagination });
+
 import '../scss/style.scss';
 
-import './main.js';
-import './modal-window.js';
-import './companies.js';
 
 
+const btnShowMore = document.querySelector('.button__show-more');
+const textMore = document.querySelector('.main-row-text--hidden');
+const textMoreHidden = document.querySelector('.main-row__sup-descr');
+
+btnShowMore.addEventListener ('click', function (){
+
+  if (textMore.style.display === 'block') {
+
+    textMore.style.display = 'none';
+    btnShowMore.innerHTML = 'Читать далее';
+    btnShowMore.classList.toggle('button-show-more--active');
+
+    if (window.innerWidth >= 768) {
+
+      textMoreHidden.style.display = 'block';
+
+    }
+
+  } else {
+
+    textMore.style.display = 'block';
+    btnShowMore.innerHTML = 'Скрыть';
+    btnShowMore.classList.toggle('button-show-more--active');
+
+    if (window.innerWidth >= 768) {
+
+      textMoreHidden.style.display = 'none';
+
+    }
+  }
+});
+
+
+const burgerButton = document.querySelector('.button');
+const modalWindow = document.querySelector('.modal-window');
+const asideBlock = modalWindow.querySelector('.aside');
+
+const closeButton = asideBlock.querySelector('.button');
+const overlay = document.querySelector('.overlay');
+
+burgerButton.addEventListener ('click', function (){
+
+  overlay.style.display = 'block';
+  asideBlock.classList.add('aside--open');
+
+});
+
+closeButton.addEventListener ('click', function (){
+
+  overlay.style.display = 'none';
+  asideBlock.classList.remove('aside--open');
+
+});
+
+window.addEventListener ('click', function (e){
+
+  if (e.target == overlay) {
+
+    overlay.style.display = 'none';
+    asideBlock.classList.remove('aside--open');
+    modalMessage.classList.remove('feedback--open');
+    modalPhone.classList.remove('call--open');
+    
+  }
+});
+
+
+
+let swiper1
+let swiper2
+let swiper3
+let swiper4
+
+document.addEventListener('DOMContentLoaded', () => {
+  const mobile = window.matchMedia('(min-width: 0px) and (max-width: 767px)');
+  const laptop = window.matchMedia('(min-width: 0px) and (max-width: 1119px)');
+
+  function swiperInit() {
+
+    if (mobile.matches) {
+      swiper4 = new Swiper('.prices-swiper', {
+        loop: true,
+        slidesPerView: 'auto',
+        spaceBetween: 16,
+      
+        modules: [Pagination],
+        pagination: {
+            el: '.swiper-pagination',
+        },
+      });
+    }
+
+    if (mobile.matches) {
+      swiper3 = new Swiper('.repair-swiper', {
+        loop: true,
+        slidesPerView: 'auto',
+        spaceBetween: 16,
+      
+        modules: [Pagination],
+        pagination: {
+            el: '.swiper-pagination',
+        },
+      });
+    }
+
+    if (mobile.matches) {
+      swiper1 = new Swiper('.companies-swiper', {
+        loop: true,
+        slidesPerView: 'auto',
+      
+        modules: [Pagination],
+        pagination: {
+            el: '.swiper-pagination',
+        },
+      });
+    }
+
+    if (laptop.matches) {
+      swiper2 = new Swiper('.main__swiper', {
+        loop: true,
+        slidesPerView: 'auto'
+      });
+    }
+  }
+
+  swiperInit()
+});
+
+
+
+const companiesBtnShowMore = document.querySelector('.companies-button-show-more');
+const companiesBlock = document.querySelector('.more');
+
+companiesBtnShowMore.addEventListener ('click', function (){
+
+  if (companiesBlock.style.display === 'grid') {
+
+    companiesBlock.style.display = 'none';
+    companiesBtnShowMore.innerHTML = 'Показать всё';
+    companiesBtnShowMore.classList.toggle('button-show-more--active');
+
+  } else {
+
+    companiesBlock.style.display = 'grid';
+    companiesBtnShowMore.innerHTML = 'Скрыть';
+    companiesBtnShowMore.classList.toggle('button-show-more--active');
+
+  }
+});
+
+
+const repairBtnShowMore = document.querySelector('.repair-button-show-more');
+const repairBlock = document.querySelector('.repair-more');
+
+repairBtnShowMore.addEventListener ('click', function (){
+  
+  if (repairBlock.style.display === 'grid') {
+
+    repairBlock.style.display = 'none';
+    repairBtnShowMore.innerHTML = 'Показать всё';
+    repairBtnShowMore.classList.toggle('button-show-more--active');
+
+  } else {
+
+    repairBlock.style.display = 'grid';
+    repairBtnShowMore.innerHTML = 'Скрыть';
+    repairBtnShowMore.classList.toggle('button-show-more--active');
+
+  }
+});
+
+
+
+
+
+
+
+
+
+
+//  модал телефон
+
+
+const btnPhone = document.querySelector('.button-phone');
+const btnPhoneBurger = document.querySelector('.button-phone-burger');
+const btnPhoneClose = document.querySelector('.call__btn-close');
+
+const modalPhone = document.querySelector('.call');
+
+
+
+btnPhone.addEventListener ('click', function () {
+  
+  modalPhone.classList.add('call--open');
+
+});
+
+btnPhoneBurger.addEventListener ('click', function () {
+  
+  modalPhone.classList.add('call--open');
+
+  btnPhoneClose.classList.add('call__btn-close--open');
+
+  if (window.innerWidth >= 1120) {
+    btnPhoneClose.style.display = 'flex';
+  }
+  
+
+});
+
+btnPhoneClose.addEventListener ('click', function (){
+
+  modalPhone.classList.remove('call--open');
+
+  if (window.innerWidth >= 1120) {
+    btnPhoneClose.style.display = 'none';
+  }
+
+});
+
+
+
+const btnMessage = document.querySelector('.button-message');
+const btnMessageBurger = document.querySelector('.button-message-burger');
+const btnMessageClose = document.querySelector('.feedback__btn-close');
+
+const modalMessage = document.querySelector('.feedback');
+
+
+
+btnMessage.addEventListener ('click', function () {
+  
+  modalMessage.classList.add('feedback--open');
+
+});
+
+btnMessageBurger.addEventListener ('click', function () {
+  
+  modalMessage.classList.add('feedback--open');
+
+  btnMessageClose.classList.add('feedback__btn-close--open');
+
+  if (window.innerWidth >= 1120) {
+    btnMessageClose.style.display = 'flex';
+  }
+
+});
+
+btnMessageClose.addEventListener ('click', function (){
+
+  modalMessage.classList.remove('feedback--open');
+
+  if (window.innerWidth >= 1120) {
+    btnMessageClose.style.display = 'none';
+  }
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,92 +1,105 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+let mode = 'development';
+
+
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+
+function isProd(prod, dev) {
+  return (mode === 'production' ? prod : dev);
+}
 
 module.exports = {
-  // Входной файл
-  entry: [
-    './src/js/index.js'
-  ],
-
-  // Выходной файл
-  output: {
-    filename: './js/bundle.js'
+  mode: mode,
+  entry: {
+    main: path.resolve(__dirname, 'src/js/index.js'),
   },
-
-  // Source maps для удобства отладки
-  devtool: "source-map",
-
+  output: {
+    filename: isProd('[name].[contenthash].js', '[name].js'),
+    assetModuleFilename: 'assets/[contenthash][ext][query]',
+    clean: true,
+  },
+  devServer: {
+    open: true,
+    static: {
+      directory: path.resolve(__dirname, 'src'),
+      watch: true,
+    },
+  },
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+  },
+  devtool: 'source-map',
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: isProd('[name].[contenthash].css', '[name].css'),
+    }),
+  ],
   module: {
     rules: [
-      // Транспилируем js с babel
       {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src/js'),
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          isProd(MiniCssExtractPlugin.loader, 'style-loader'),
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'postcss-preset-env',
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp|awif|ico)$/i,
+        type: isProd('asset/resource','asset/inline') ,
+        generator: isProd({filename: 'assets/img/[hash][ext][query]'}, {})
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[hash][ext][query]',
+        },
+      },
+      {
+        test: /\.m?js$/i,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-          }
-        }
-      },
-
-      // Компилируем SCSS в CSS
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader, // Extract css to separate file
-          'css-loader', // translates CSS into CommonJS
-          'postcss-loader', // parse CSS and add vendor prefixes to CSS rules
-          'sass-loader', // compiles Sass to CSS, using Node Sass by default
-        ],
-      },
-
-      // Подключаем шрифты из css
-      {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader?name=./fonts/[name].[ext]'
           },
-        ]
-      },
-
-      // Подключаем картинки из css
-      {
-        test: /\.(svg|png|jpg|jpeg|webp)$/,
-        use: [
-          {
-            loader: 'file-loader?name=./static/[name].[ext]'
-          },
-        ]
+        },
       },
     ],
   },
-  plugins: [
-    // Подключаем файл html, стили и скрипты встроятся автоматически
-    new HtmlWebpackPlugin({
-      title: 'Webpack 4 Starter',
-      template: './src/index.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: false,
-      }
-    }),
-
-    // Кладем стили в отдельный файлик
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-
-    // Копируем картинки
-    new CopyWebpackPlugin([
-      {
-        from: './src/img',
-        to: 'img',
-      },
-    ])
-  ],
 };
